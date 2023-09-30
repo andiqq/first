6f2e1d12c3d8c613147687f71f16dd67ba7a52ea
Dr. Andreas Plagens <andreas@plagens.eu>
2023-09-30 22:52:05 +0200

    Initial Commit

---
+++ first/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first
+//
+//  Created by Dr. Andreas Plagens on 30.09.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
