1b917bf600cae597193afec3dd0fe8c4b50dd8f7
Dr. Andreas Plagens <andreas@plagens.eu>
2023-09-30 22:58:09 +0200

    typo

--- first/main.cpp
+++ first/main.cpp
@@ -11,7 +11,7 @@ int main()
 
 {
     int rate;
-    std::cout << "How would you rate this book so for? Please rate from 1 - 10" << std::endl;
+    std::cout << "How would you rate this book so far? Please rate from 1 - 10" << std::endl;
     std::cin >> rate;
     std::cout << "You rated this book as " << rate << ", thanks for your feedback!" << std::endl;
 }
